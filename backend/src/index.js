e61d6e75e681decb015e4eeb74162b32dd2f7e81
123Marius321 <92578022+123Marius321@users.noreply.github.com>
2022-03-30 12:29:45 +0200

    Revert "Docker eingerichtet"
    This reverts commit 83a852dcc1cae166d377bced445a9d4e902c01d2.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import foodRoutes from "./routes/foodRoutes.js";
-import mongoose from "mongoose";
 
 const app = express();
 const port = 4000;
@@ -10,10 +9,6 @@ app.use(bodyParser.json());
 
 app.use("/food", foodRoutes);
 app.all("*", (req, res) => res.sendStatus(404));
-
-mongoose.connect("mongodb://mongo:27017/test").then(() => {
-  console.log("Database connected");
-});
 
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
--- backend/src/models/food.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const foodSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    course: String,
-});
-
-export const Food = mongoose.model("Food", foodSchema);
